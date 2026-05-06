5aa31e139e3179a2b08c35d3518d414a0fd6fc06
Norah Saeed <norahseedw@gmail.com>
2026-05-06 11:38:59 +0300

    resolve payment

--- FreeLanzo/payment/views.py
+++ FreeLanzo/payment/views.py
@@ -1,10 +1,12 @@
+from django.shortcuts import render
 import stripe
 from django.http import HttpRequest, HttpResponse
 from django.conf import settings
 from django.shortcuts import redirect, get_object_or_404
-from accounts.models import Project
-from django.contrib import messages
+from accounts.models import Project  
 
+def payment_view(request):
+    return render(request, 'payment/payment.html')
 
 # Create your views here.
 
@@ -14,42 +16,24 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 def create_checkout_session(request:HttpRequest, project_id):
     project = get_object_or_404(Project, id=project_id)
 
-    if request.user != project.client:
-        messages.error(request, "You are not allowed to pay for this project.", "alert-danger")
-        return redirect('accounts:project_detail', project_id=project.id)
-    
-    if project.status == 'completed':
-        messages.warning(request, "This project is already completed." , "alert-warning")
-        return redirect('accounts:project_detail', project_id=project.id)
-    try:
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': project.title,
-                    },
-                    'unit_amount': int(project.budget * 100),
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': project.title,
                 },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=request.build_absolute_uri('/payment/success/'),
-            cancel_url=request.build_absolute_uri('/payment/cancel/'),
-            metadata={
-                "project_id": project.id
-            }
-        )
+                'unit_amount': int(project.budget * 100),
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://127.0.0.1:8000/success/',
+        cancel_url='http://127.0.0.1:8000/cancel/',
+        metadata={
+            "project_id": project.id
+        }
+    )
 
-        return redirect(session.url)
-    except Exception as e:
-        messages.error(request, "Payment session failed. Please try again." , "alert-danger")
-        return redirect('accounts:project_detail', project_id=project.id)
-
-
-def payment_success(request:HttpRequest):
-    return HttpResponse("Payment Successful ")
-
-def payment_cancel(request:HttpRequest):
-    return HttpResponse("Payment Cancelled ")
+    return redirect(session.url)
